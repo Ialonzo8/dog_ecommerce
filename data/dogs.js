b089d1410a7ef06a76e5beb4be4aed565352650c
Antonio <asoriano@appdev.yearup.org>
2024-07-02 09:07:10 -0500

    add array for dog data

---
+++ data/dogs.js
@@ -0,0 +1,46 @@
+"use strict"
+
+let dogs = [
+    {
+        title: "male",
+        price: 3000,
+        gender: "male",
+        color: "black",
+        img: "images/dog pic 3.jpg"
+    },
+    {
+        title: "female",
+        price: 2000,
+        gender: "female",
+        color: "black",
+        img: "images/dog pic 4.jpg"
+    },
+    {
+        title: "Black",
+        price: 3000,
+        gender: "male",
+        color: "black",
+        img: "images/dog pic 5.jpg"
+    },
+    {
+        title: "Fawn",
+        price: 4000,
+        gender: "female",
+        color: "brown",
+        img: "images/dog pic 6.jpg"
+    },
+    {
+        title: "Blue",
+        price: 4000,
+        gender: "female",
+        color: "black",
+        img: "images/dog pic10.jpg"
+    },
+    {
+        title: "Blue Merle",
+        price: 10000,
+        gender: "male",
+        color: "white",
+        img: "images/dogpic14.jpg"
+    }
+];
